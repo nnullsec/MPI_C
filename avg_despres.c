7b156a60316e6f1e7d51eeeb1f3644f7830bff42
post-mental <40575764+post-mental@users.noreply.github.com>
2019-11-30 20:26:43 +0200

    features added
    - user has the option to kill or continue executing the program

--- avg_despres.c
+++ avg_despres.c
@@ -3,8 +3,8 @@
 
 void main(int argc, char** argv){
     int my_rank;
-    int k,res,finres,num_of_elements,p,n;
-    int source,target;
+    int k,res,finres,num_of_elements, p, n;
+    int source,target, count;
     int tag1=1, tag2=2;
     int min_l,max_l,var,j;
     float average;
@@ -12,8 +12,6 @@ void main(int argc, char** argv){
     float D[100];
 
     char option;
-    unsigned short int counter = 0;
-
 
     MPI_Status status;
     MPI_Init(&argc, &argv);
@@ -22,132 +20,154 @@ void main(int argc, char** argv){
 
 
 /*********************************************************************
-******************************           *****************************
+****************************** Erwthma 1 *****************************
 **********************************************************************/
+    option = '1';
+    count = 0;
 
 
-    if(my_rank == 0){
-        printf("\nGive the amount of numbers (up to 100): ");
-        scanf("%d", &n);
-        //n=8; //debug
-        printf("Give %d numbers: ", n);
+    while (option == '1') {
 
-        for (j=0; j<n; j++)
-            scanf("%d", &arrayA[j]);
+        if(my_rank == 0){
 
-        for (target = 1; target < p; target++){
-            MPI_Send(&n, 1, MPI_INT, target, tag1, MPI_COMM_WORLD);
+            if (count++){
+                for (j=0; j<50; j++)
+                    printf("=");
+                printf("\n\t\t* No. of runs: %d *\n",count);
+            }
 
+            printf("\nGive amount of numbers (up to 100): ");
+            scanf("%d", &n);
+            //n=8; //debug
+            printf("Insert %d numbers: ", n);
+
+            for (j=0; j<n; j++)
+                scanf("%d", &arrayA[j]);
+
+            for (target = 1; target < p; target++){
+                MPI_Send(&n, 1, MPI_INT, target, tag1, MPI_COMM_WORLD);
+
+                num_of_elements = n/p;
+                k = num_of_elements;
+            }
+            for (target = 1; target < p; target++){
+                MPI_Send(&arrayA[k], num_of_elements, MPI_INT, target, tag2, MPI_COMM_WORLD);
+                k += num_of_elements;
+            }
+        }
+        else{
+            MPI_Recv(&n, 1, MPI_INT, 0, tag1, MPI_COMM_WORLD, &status);
             num_of_elements = n/p;
-            k = num_of_elements;
-        }
-        for (target = 1; target < p; target++){
-            MPI_Send(&arrayA[k], num_of_elements, MPI_INT, target, tag2, MPI_COMM_WORLD);
-            k += num_of_elements;
-        }
-    }
-    else{
-        MPI_Recv(&n, 1, MPI_INT, 0, tag1, MPI_COMM_WORLD, &status);
-        num_of_elements = n/p;
-        MPI_Recv(&arrayA[0], num_of_elements, MPI_INT, 0, tag2, MPI_COMM_WORLD, &status);
-    }
-
-    for(k=0; k<num_of_elements; k++)
-        res = res + (arrayA[k]);
-
-    if (my_rank == 0)
-        printf("\n\t** Erwthma 1 **\n");
-
-    if (my_rank != 0)
-        MPI_Send(&res, 1, MPI_INT, 0, tag1, MPI_COMM_WORLD);
-    else{
-        finres = res;
-        printf(" proccess %d result: %d\n", my_rank, res);
-
-        for (source = 1; source < p; source++){
-            MPI_Recv(&res, 1, MPI_INT, source, tag1, MPI_COMM_WORLD, &status);
-            finres = finres + res;
-            printf(" proccess %d result: %d\n", source, res);
-       }
-
-        printf("\tSum: %d \n",finres);
-        average = finres / n;
-        printf("\tAverage: %d/%d = %.1f\n\n",finres,n, (float)finres / n);
-    }
-
-/*********************************************************************
-******************************           *****************************
-**********************************************************************/
-
-    if (my_rank==0){
-        for(target = 1; target < p; target++)
-            MPI_Send(&average, 1, MPI_INT, target, tag1, MPI_COMM_WORLD);
-    }
-    else
-        MPI_Recv(&average,1,MPI_INT,0,tag1,MPI_COMM_WORLD, &status);
-
-    for (k=0; k<num_of_elements; k++)
-        res = res + ((arrayA[k]-average)*(arrayA[k]-average));
-
-    if (my_rank == 0)
-        printf("\n\t** Erwthma 2 **\n");
-
-    if (my_rank != 0)
-        MPI_Send(&res, 1, MPI_INT, 0, tag1, MPI_COMM_WORLD);
-    else{
-        finres = res;
-        printf(" proccess %d result: %d\n", my_rank, res);
-
-        for (source = 1; source < p; source++){
-           MPI_Recv(&res, 1, MPI_INT, source, tag1, MPI_COMM_WORLD, &status);
-           finres = finres + res;
-           printf(" proccess %d result: %d\n", source, res);
+            MPI_Recv(&arrayA[0], num_of_elements, MPI_INT, 0, tag2, MPI_COMM_WORLD, &status);
         }
 
-        printf("\tdespresion: %d/%d = %.1f\n\n",finres,n, (float)finres / n);
-    }
+        for(k=0; k<num_of_elements; k++)
+            res = res + (arrayA[k]);
 
-/*********************************************************************
-******************************           *****************************
-**********************************************************************/
+        if (my_rank == 0)
+            printf("\n\t** Erwthma 1 **\n");
 
+        if (my_rank != 0)
+            MPI_Send(&res, 1, MPI_INT, 0, tag1, MPI_COMM_WORLD);
+        else{
+            finres = res;
+            printf(" proccess %d result: %d\n", my_rank, res);
 
-    if (my_rank == 0){
-        max_l = arrayA[0];
-        min_l = arrayA[0];
+            for (source = 1; source < p; source++){
+                MPI_Recv(&res, 1, MPI_INT, source, tag1, MPI_COMM_WORLD, &status);
+                finres = finres + res;
+                printf(" proccess %d result: %d\n", source, res);
+           }
 
-        for (k=0; k<n; k++){
-            if (arrayA[k] > max_l)
-                max_l = arrayA[k];
-            if (arrayA[k] < min_l)
-                min_l = arrayA[k];
+            printf("\tSum: %d \n",finres);
+            average = finres / n;
+            printf("\tAverage: %d/%d = %.1f\n\n",finres,n, (float)finres / n);
         }
 
-        for(target = 1; target < p; target++){
-            MPI_Send(&min_l, 1, MPI_INT, target, tag1, MPI_COMM_WORLD);
-            MPI_Send(&max_l, 1, MPI_INT, target, tag2, MPI_COMM_WORLD);
+    /*********************************************************************
+    ****************************** Erwthma 2 *****************************
+    **********************************************************************/
+
+        if (my_rank==0){
+            for(target = 1; target < p; target++)
+                MPI_Send(&average, 1, MPI_INT, target, tag1, MPI_COMM_WORLD);
         }
-    }
+        else
+            MPI_Recv(&average,1,MPI_INT,0,tag1,MPI_COMM_WORLD, &status);
 
-    if (my_rank!=0){
-        MPI_Recv(&min_l, 1, MPI_INT, 0, tag1, MPI_COMM_WORLD, &status);
-        MPI_Recv(&max_l, 1, MPI_INT, 0, tag2, MPI_COMM_WORLD, &status);
-
-        for (k=0; k<num_of_elements; k++){
-            D[k] = ((arrayA[k] - min_l) / (float)(max_l - min_l)) * 100;
-        }
-        MPI_Send(D, num_of_elements, MPI_FLOAT, 0, tag1, MPI_COMM_WORLD);
-    }
-    if (my_rank == 0)
-        printf("\n\t** Erwthma 3 **\n");
-
-    if (my_rank == 0){
         for (k=0; k<num_of_elements; k++)
-            D[k] = ((arrayA[k] - min_l) / (float)(max_l - min_l)) * 100;
-        for (source = 1; source < p; source++)
-            MPI_Recv(D+source*num_of_elements, num_of_elements, MPI_FLOAT, source, tag1, MPI_COMM_WORLD, &status);
-        for (k=0; k<n; k++)
-            printf("d[%d]: %3.1f%%\n", k, D[k]);
+            res = res + ((arrayA[k]-average)*(arrayA[k]-average));
+
+        if (my_rank == 0)
+            printf("\n\t** Erwthma 2 **\n");
+
+        if (my_rank != 0)
+            MPI_Send(&res, 1, MPI_INT, 0, tag1, MPI_COMM_WORLD);
+        else{
+            finres = res;
+            printf(" proccess %d result: %d\n", my_rank, res);
+
+            for (source = 1; source < p; source++){
+               MPI_Recv(&res, 1, MPI_INT, source, tag1, MPI_COMM_WORLD, &status);
+               finres = finres + res;
+               printf(" proccess %d result: %d\n", source, res);
+            }
+
+            printf("\tdespresion: %d/%d = %.1f\n\n",finres,n, (float)finres / n);
+        }
+
+    /*********************************************************************
+    ****************************** Erwthma 3 *****************************
+    **********************************************************************/
+
+
+        if (my_rank == 0){
+            max_l = arrayA[0];
+            min_l = arrayA[0];
+
+            for (k=0; k<n; k++){
+                if (arrayA[k] > max_l)
+                    max_l = arrayA[k];
+                if (arrayA[k] < min_l)
+                    min_l = arrayA[k];
+            }
+
+            for(target = 1; target < p; target++){
+                MPI_Send(&min_l, 1, MPI_INT, target, tag1, MPI_COMM_WORLD);
+                MPI_Send(&max_l, 1, MPI_INT, target, tag2, MPI_COMM_WORLD);
+            }
+        }
+
+        if (my_rank!=0){
+            MPI_Recv(&min_l, 1, MPI_INT, 0, tag1, MPI_COMM_WORLD, &status);
+            MPI_Recv(&max_l, 1, MPI_INT, 0, tag2, MPI_COMM_WORLD, &status);
+
+            for (k=0; k<num_of_elements; k++){
+                D[k] = ((arrayA[k] - min_l) / (float)(max_l - min_l)) * 100;
+            }
+            MPI_Send(D, num_of_elements, MPI_FLOAT, 0, tag1, MPI_COMM_WORLD);
+        }
+        if (my_rank == 0)
+            printf("\n\t** Erwthma 3 **\n");
+
+        if (my_rank == 0){
+            for (k=0; k<num_of_elements; k++)
+                D[k] = ((arrayA[k] - min_l) / (float)(max_l - min_l)) * 100;
+            for (source = 1; source < p; source++)
+                MPI_Recv(D+source*num_of_elements, num_of_elements, MPI_FLOAT, source, tag1, MPI_COMM_WORLD, &status);
+            for (k=0; k<n; k++)
+                printf("d[%d]: %3.1f%%\n", k, D[k]);
+        }
+
+
+            if (my_rank == 0) {
+                printf("\n\n\t\t1. Continue\n\t\t2. Kill\n\t\tPlease enter your option: ");
+                scanf(" %c", &option);
+                printf("\n\n");
+                for (target = 1; target < p; target++)
+                    MPI_Send(&option, 1, MPI_INT, target, tag1, MPI_COMM_WORLD);
+            } else
+                MPI_Recv(&option, 1, MPI_INT, 0, tag1, MPI_COMM_WORLD, &status);
     }
 
     MPI_Finalize();
