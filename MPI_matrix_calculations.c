320ae7280df07edda5a276f30a44f4f92deb73cd
post-mental <40575764+post-mental@users.noreply.github.com>
2019-12-27 22:30:21 +0200

    Update MPI_matrix_calculations.c
    minor bugfixes
    matrix sum now works with arbitrary number of processes/elements

--- MPI_matrix_calculations.c
+++ MPI_matrix_calculations.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include "mpi.h"
-#define N 3
-
+#define N 4
 int main(int argc, char** argv){
     int my_rank,p,intsz;
     int arrA[N],arrB[N],arrC[N][N],arrD[N][N];
-    int tmparrA[N]={0},tmparrB[N]={0},tmparrC[N][N]={0},locA;
-    int i,j,sum=0;
+    int tmparrA[N*N]={0}, tmparrB[N*N]={0}, locA;
+    int i,j,sum=0,nduty=1;
     char option='0';
 
     MPI_Status status;
@@ -18,47 +17,61 @@ int main(int argc, char** argv){
         if(my_rank == 0){
             int cntr0=1,cntr1=1;
             
-            if(p>N) return -1;
-            
+            if(N%p){// if p does not divide N
+                printf("\ninvalid # of processes: returning with -1\n");
+                return -1;
+            }
+            else if(N!=p)
+                nduty=N/p;
+
             for(i=0; i<N; i++){
                 for(j=0; j<N; j++){
                     arrC[i][j]=cntr1;
                     arrD[i][j]=cntr1++;
                 }
             }
-
-            printf(" 1. C+D\n 2. C*B\n 3. A*B\nInput: ");
+            printf("\n---- Program starts here ----\n");
+            printf(" 1. C+D\n 2. C*B\n 3. A*B\n Input: ");
             scanf(" %c",&option);
 
             if(option=='3'){
-                printf("\n========   A   ========\n");
+                if(N!=p){
+                    printf("\nUnfortunatelly this won't work...\n3rd option requires N == p\n\n");
+                    return -1;
+                }
+                printf("\n===========   A   ===========\n");
                 for(i=0; i<N; i++,cntr0++){
                     arrA[i]=cntr0; 
-                    printf("%d ",arrA[i]);
+                    printf("%3d ",arrA[i]);
                 }
             }
             if(option=='2' || option=='3'){
-                printf("\n========   B   ========\n");
+                if(option=='2')
+                    if(N!=p){
+                        printf("\nUnfortunatelly this won't work...\n2nd option requires N == p\n\n");
+                        return -1;
+                    }
+                printf("\n===========   B   ===========\n");
                 for(i=0,cntr0=N+1; i<N; i++,cntr0++){                
                     arrB[i]=cntr0;
-                    printf("%d\n", arrB[i]);
+                    printf("%3d\n", arrB[i]);
                 }
             }
             if(option=='1' || option=='2'){
-                printf("\n========   C   ========\n");
+                printf("\n===========   C   ===========\n");
                 for(i=0; i<N; i++){
                     for(j=0; j<N; j++){
-                        printf("%2d ",arrC[i][j]);
+                        printf("%3d ",arrC[i][j]);
                         if(j == N-1)
                             printf("\n");
                     }
                 }
             }
             if(option=='1'){
-                printf("\n========   D   ========\n");
+                printf("\n===========   D   ===========\n");
                 for(i=0; i<N; i++){
                     for(j=0; j<N; j++){
-                        printf("%2d ",arrD[i][j]);
+                        printf("%3d ",arrD[i][j]);
                         if(j == N-1)
                             printf("\n");
                     }
@@ -66,33 +79,37 @@ int main(int argc, char** argv){
             }
         }
 MPI_Bcast(&option, 1, MPI_CHAR, 0, MPI_COMM_WORLD);
+MPI_Bcast(&nduty, 1, MPI_CHAR, 0, MPI_COMM_WORLD);
+
 /*************************************************
 ******************* erwthma 1 ********************
 *************************************************/
+
         if(option=='1'){
-            MPI_Scatter(arrC, N, MPI_INT, tmparrA, N, MPI_INT, 0, MPI_COMM_WORLD);
-            MPI_Scatter(arrD, N, MPI_INT, tmparrB, N, MPI_INT, 0, MPI_COMM_WORLD);
+            MPI_Scatter(arrC, N*nduty, MPI_INT, tmparrA, N*nduty, MPI_INT, 0, MPI_COMM_WORLD);
+            MPI_Scatter(arrD, N*nduty, MPI_INT, tmparrB, N*nduty, MPI_INT, 0, MPI_COMM_WORLD);
 
-            for(i=0;i<N;i++)
+            for(i=0;i<N*nduty;i++)
                 tmparrA[i]+=tmparrB[i];
-
-            MPI_Gather(tmparrA, N, MPI_INT, arrD, N, MPI_INT, 0, MPI_COMM_WORLD);
+            
+            MPI_Gather(tmparrA, N*nduty, MPI_INT, arrD, N*nduty, MPI_INT, 0, MPI_COMM_WORLD);
+            
             if(my_rank==0){
-                printf("\n========  C+D  ========\n");
+                printf("\n===========  C+D  ===========\n");
                 for(i=0; i<N; i++){
                     for(j=0; j<N; j++){
-                        printf("%2d ",arrD[i][j]);
+                        printf("%3d ",arrD[i][j]);
                     if(j == N-1)
                         printf("\n");
                     }
                 }
             }
         }
-MPI_Barrier(MPI_COMM_WORLD);
 
 /*************************************************
 ******************* erwthma 2 ********************
 *************************************************/
+
         if(option=='2'){
             locA=0;
 
@@ -100,43 +117,49 @@ MPI_Barrier(MPI_COMM_WORLD);
             MPI_Bcast(arrB, N, MPI_INT, 0, MPI_COMM_WORLD);
             
             for(i=0; i<N; i++)
-                locA += arrB[i] * tmparrA[i];
+                    locA += arrB[i] * tmparrA[i];
 
             MPI_Gather(&locA, 1, MPI_INT, tmparrB, 1, MPI_INT, 0, MPI_COMM_WORLD);
             
             if (my_rank == 0){
-                printf("\n========  C*B  ========\n");
+                printf("\n===========  C*B  ============\n");
                 for (i=0; i<N; i++){
-                    printf("%d\n", tmparrB[i]);
+                    printf("%3d\n", tmparrB[i]);
                 }
             }
         }
+
 /*************************************************
 ******************* erwthma 3 ********************
 *************************************************/
 
         if(option=='3'){
+            sum=0;
+
             MPI_Scatter(arrA, N/p, MPI_INT, tmparrA, N/p, MPI_INT, 0, MPI_COMM_WORLD);
             MPI_Scatter(arrB, N/p, MPI_INT, tmparrB, N/p, MPI_INT, 0, MPI_COMM_WORLD);
 
             for(i=0;i<N/p;i++)
                 tmparrA[i]*=tmparrB[i];
-            sum=0;
+            
             MPI_Reduce(tmparrA, &sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
             if(my_rank==0){
-                printf("\n========  A*B  ========\n");
-                printf("\t   %d\n", sum);
+                printf("\n===========  A*B  ============\n");
+                printf("%3d\n", sum);
             }
         }
 
 
+/************************************************************/
         if(my_rank==0){
-            while(option=!'0' || option!='1'){
-                printf("\tPress: 0 for exit\n\tPress: 1 to repeat\n\tInput: ");
+            while(1){
+                printf("\n---- Program ends here ----\n");
+                printf(" Press: 0 to terminate\n Press: 1 to repeat \n Input: ");
                 scanf(" %c",&option);
+                printf("\n\n\n");
                 if(option=='0')  return 0;
-                else if(option=='1') continue;
+                else if(option=='1') break;
             }
         }
     }
